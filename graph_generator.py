bug fixed in lin pol clean flux calculation

graph_generator.py
@@ -395,9 +395,9 @@ class ImageData(object):
             self.integrated_flux_clean = 0
         #and then polarization
         try:
-            flux_q=total_flux_from_mod("tmp/mod_files_q/" + self.date + "_" + "{:.0f}".format(self.freq/1e9).replace(".","_") + "GHz.mod")
-            flux_u=total_flux_from_mod("tmp/mod_files_u/" + self.date + "_" + "{:.0f}".format(self.freq/1e9).replace(".","_") + "GHz.mod")
-            self.integrated_pol_flux_clean=np.sqrt(flux_u**2+flux_q**2)
+            flux_q_squared=total_flux_from_mod("tmp/mod_files_q/" + self.date + "_" + "{:.0f}".format(self.freq/1e9).replace(".","_") + "GHz.mod",squared=True)
+            flux_u_squared=total_flux_from_mod("tmp/mod_files_u/" + self.date + "_" + "{:.0f}".format(self.freq/1e9).replace(".","_") + "GHz.mod",squared=True)
+            self.integrated_pol_flux_clean=np.sqrt(flux_u_squared+flux_q_squared)
         except:
             self.integrated_pol_flux_clean=0
 
@@ -1043,13 +1043,16 @@ def get_date(filename):
     return date
 
 #needs a mod_file as input an returns the total flux
-def total_flux_from_mod(mod_file):
+def total_flux_from_mod(mod_file,squared=False):
     lines=open(mod_file).readlines()
     total_flux=0
     for line in lines:
         if not line.startswith("!"):
             linepart=line.split()
-            total_flux+=float(linepart[0])
+            if squard:
+                total_flux+=float(linepart[0])**2
+            else:
+                total_flux+=float(linepart[0])
     return total_flux
                 
 def PXPERBEAM(b_maj,b_min,px_inc):

main.py
@@ -1491,7 +1491,7 @@ class ModelFits(TabbedPanel):
         self.ids.stokes_i_noise.text = "{:.2f}".format(image.noise*1000) + " mJy/beam"
         self.ids.pol_noise.text = "{:.2f}".format(image.pol_noise*1000) + " mJy/beam"
         self.ids.evpa.text = "{:.2f}".format(image.evpa_average/np.pi*180) + "°"
-        self.ids.beam_text.text = "{:.2f}".format(image.beam_maj) + " mas, " + "{:.2f}".format(image.beam_min) + " mas, " + "{:.2f}".format(image.beam_pa)+"°"
+        self.ids.beam_text.text = "{:.2f}".format(image.beam_maj) + " mas x " + "{:.2f}".format(image.beam_min) + " mas, " + "{:.2f}".format(image.beam_pa)+"°"
         if image.integrated_flux_clean!=0:
             self.ids.fractional_polarization.text = (
                     "{:.2f}".format(image.integrated_pol_flux_clean/image.integrated_flux_clean*100)+"%")